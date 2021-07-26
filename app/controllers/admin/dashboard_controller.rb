b6227c7ebc51f428c8df17b5c080d9890494421d
Adam Underhill <adameunderhill@gmail.com>
2021-07-26 14:36:12 -0700

    adding the views for admin categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,9 +3,12 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
-    @product_count = Product.all.product_count
-    @apparel_count = Product.where(category_id: 1).count
-    @electronic_count = Product.where(category_id: 2).count
-    @furniture_count = Product.where(category_id: 3).count
+    #@product_count = Product.all.product_count
+    #@apparel_count = Product.where(category_id: 1).count
+    #@electronic_count = Product.where(category_id: 2).count
+    #@furniture_count = Product.where(category_id: 3).count
+
+    @category = Category.all
+    @product = Product.all
   end
 end
