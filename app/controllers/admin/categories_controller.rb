26e3dacfedd3d4b8579855faefa37646fa9e9e5c
Adam Underhill <adameunderhill@gmail.com>
2021-07-26 14:56:38 -0700

    add functionality and views for the admin to create new categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,25 +1,39 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+
   def index
     @category_name_to_id = {}
     @product_per_category_count = {}
+    @categories = Category.all
     @product_category = Category.joins(:products)
 
+    @categories.each do |x|
+      @product_per_category_count[x.name] = 0
+      @category_name_to_id[x.name] = x.id
+    end
+
     @product_category.map do |i|
-      unless @product_per_category_count[i.name] == nil
-        @product_per_category_count[i.name] += 1
-      else
-        @product_per_category_count[i.name] = 1
         @category_name_to_id[i.name] = i.id
       end
     end
   end
 
-  def
-    new
+  def new
+    @category = Category.new
   end
 
-  def 
-    create
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Product category has been created!'
+    else
+      render :new
+
+    private
+
+    def category_params
+      params.require(:category).permit(:name)
   end
 end
