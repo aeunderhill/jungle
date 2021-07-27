96217eefa8973e4c331289e5739eadba8f0d28ad
Adam Underhill <adameunderhill@gmail.com>
2021-07-27 13:11:24 -0700

    setting up rspec test environment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210727123456) do
+ActiveRecord::Schema.define(version: 20210727200423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,16 @@ ActiveRecord::Schema.define(version: 20210727123456) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "widgets", force: :cascade do |t|
+    t.string   "a"
+    t.string   "g"
+    t.string   "is"
+    t.string   "for"
+    t.string   "generate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "line_items", "orders"
