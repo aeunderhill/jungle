2acf91ca72aaa18b99ae68734d68393f77a3d5bc
Adam Underhill <adameunderhill@gmail.com>
2021-07-27 12:55:40 -0700

    debugging admin dashboard and fixing login issues

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,24 +3,16 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def index
-    @category_name_to_id = {}
-    @product_per_category_count = {}
-    @categories = Category.all
-    @product_category = Category.joins(:products)
-
-    @categories.each do |x|
-      @product_per_category_count[x.name] = 0
-      @category_name_to_id[x.name] = x.id
+   
+    @categories = Category.joins(:products).distinct
+    #abort @categories.to_sql
     end
 
-    @product_category.map do |i|
-        @category_name_to_id[i.name] = i.id
-      end
-    end
-  end
 
   def new
+    
     @category = Category.new
+    
   end
 
   def create
@@ -30,11 +22,11 @@ class Admin::CategoriesController < ApplicationController
       redirect_to [:admin, :categories], notice: 'Product category has been created!'
     else
       render :new
-
+    end 
+  end
     private
 
     def category_params
       params.require(:category).permit(:name)
   end
-end
 end
